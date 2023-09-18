eede3e8159a8a7008800aae23b51796b7738669d
Ron Jacob <generalsmoke23@gmail.com>
2023-09-18 19:21:01 +0530

    feat: Completed Header Component Established Background-Styles

---
+++ my-website/app/layout.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Ricardo | Personal Portfolio",
+  description: "Ricardo is a full-stack developer with 8 years of experience.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50 text-gray-950 `}>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
+        <div className="bg-[#dbd7fb] absolute top-[-6rem] right-[11rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
